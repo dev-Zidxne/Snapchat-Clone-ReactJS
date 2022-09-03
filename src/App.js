ab3d6e078179c436fa3c4a1bb838e501b2d9e320
Zidane Innis <zidaneinnis@gmail.com>
2022-09-03 17:50:12 -0500

    | Building the WebcamCapture Component (2/2)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Lets build Snap</h1>
-      <WebcamCapture />
+    <div className="appy">
+      <Router>
+        <div className="app__body">
+          <Routes>
+            <Route exact path="/preview" element={<Preview />}></Route>
+
+            <Route exact path="/"></Route>
+          </Routes>
+          <WebcamCapture />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,6 +1,9 @@
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import { useDispatch } from "react-redux";
+import { setCameraImage } from "./features/cameraSlice";
+import { useNavigate } from "react-router-dom";
 const videoConstraints = {
   width: 250,
   height: 400,
@@ -9,9 +12,14 @@ const videoConstraints = {
 
 function WebcamCapture() {
   const webcamRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
+    dispatch(setCameraImage(imageSrc));
+    navigate("/preview");
   }, [webcamRef]);
   return (
     <div className="webcamCapture">
