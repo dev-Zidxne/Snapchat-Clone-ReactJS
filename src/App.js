fde1a56ec9eb6c0850d2e049d3da7257fb67aae2
Zidane Innis <zidaneinnis@gmail.com>
2022-09-03 13:19:35 -0500

    Building the WebcamCapture Component (1/2)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import WebcamCapture from "./WebcamCapture";
 
 function App() {
   return (
     <div className="app">
       <h1>Lets build Snap</h1>
+      <WebcamCapture />
     </div>
   );
 }
