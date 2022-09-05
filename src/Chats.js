4d59b9f642b1b05bae005654353bb0c081843056
Zidane Innis <zidaneinnis@gmail.com>
2022-09-05 01:32:21 -0500

    Building the Chat Component

--- src/Chats.js
+++ src/Chats.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Chats.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { Avatar } from "@mui/material";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { db } from "./firebase";
+import Chat from "./Chat";
 function Chats() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
   return (
     <div className="chats">
       <div className="chats__header">
@@ -11,8 +26,24 @@ function Chats() {
         <div className="chats__search">
           <SearchIcon />
           <input placeholder="Friends" types="text" />
-          <ChatBubbleIcon className="chats__chatIcon" />
         </div>
+        <ChatBubbleIcon className="chats__chatIcon" />
+      </div>
+      <div className="chats__posts">
+        {posts.map(
+          ({
+            id,
+            data: { profilePic, username, timestamp, imageUrl, read },
+          }) => (
+            <Chat
+              key={id}
+              profilePic={profilePic}
+              username={username}
+              timestamp={timestamp}
+              imageUrl={imageUrl}
+            />
+          )
+        )}
       </div>
     </div>
   );
