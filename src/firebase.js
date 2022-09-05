e7afbbbe2cf7b46b3147fc022f0140c2a66b33f9
Zidane Innis <zidaneinnis@gmail.com>
2022-09-04 23:37:10 -0500

    Finishing up Building the Preview Component (2/2)

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
 import firebase from "firebase/compat/app"; //v9
+import "firebase/compat/auth"; //v9
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import "firebase/compat/firestore";
+
+import "firebase/compat/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyBPzbZEJDzuq7n4mPP2eSds6zc5qO0H-XY",
   authDomain: "snapchat-clone-6ae41.firebaseapp.com",
